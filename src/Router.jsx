0388fce7c6349c279b330729df151c26f665d4cf
jihyezi <154583345+jihyezi@users.noreply.github.com>
2024-04-26 03:05:50 +0900

    feat: bottomTab 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import Home from "pages/Home/Home";
-import Alarm from "pages/Home/Alarm";
+import CustomBottomTab from "./components/CustomBottomTab";
+import Home from "./pages/Home";
+import Community from "./pages/Community";
+import Archives from "./pages/Archives";
+import Mypage from "./pages/Mypage";
+import Alarm from "pages/Alarm";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const renderTabBar = (props) => <CustomBottomTab {...props} />
 
 const MainTab = () => {
   return (
@@ -16,7 +22,10 @@ const MainTab = () => {
         headerShown: false,
       }}
     >
-      <Tab.Screen name="홈" component={Alarm} />
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Community" component={Community} />
+      <Tab.Screen name="Archives" component={Archives} />
+      <Tab.Screen name="Mypage" component={Mypage} />
     </Tab.Navigator>
   );
 };
@@ -28,9 +37,8 @@ const Router = () => {
         headerShown: false,
       }}
     >
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Alarm" component={Alarm} />
       <Stack.Screen name="MainTab" component={MainTab} />
+      <Stack.Screen name="Alarm" component={Alarm} />
     </Stack.Navigator>
   );
 };
