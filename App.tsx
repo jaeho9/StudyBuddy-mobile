d2371fee1f2148d3b1ad162652964a35721118b0
rlawlgud <cindy9905@gamil.com>
2024-05-08 16:36:52 +0900

    merge

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import Router from "./src/Router";
 
 function App(): React.JSX.Element {
+
   return (
     <NavigationContainer>
       <Router />
